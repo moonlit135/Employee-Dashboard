05b932f4268ae79b5edfcf09e51c594c6baa97a9
Pallabi Das <pallabidas8011@gmail.com>
2025-06-28 00:53:23 +0530

    Updated styling and Welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@ export default function Welcome() {
   return (
     <div className="flex items-center justify-center h-full min-h-screen bg-gray-50">
       <h1 className="text-4xl font-bold text-blue-600 font-[cursive]">
-        Hello!! Welcome
+        Hello!! Welcome("Click the sidebar to view employee data.")
       </h1>
     </div>
   );
